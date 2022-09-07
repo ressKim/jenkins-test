e2bbc1faca6cf74f2f30ecd08b1f699f968ad4d4
ressKim <sh21dlat@gmail.com>
2022-09-07 19:06:25 +0900

    test 파일 작성

---
+++ src/main/java/com/example/jenkinsstudy/JenkinsstudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsstudyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JenkinsstudyApplication.class, args);
+  }
+
+}
